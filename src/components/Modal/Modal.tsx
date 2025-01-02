8cfc6fa9e0356054cf3e15ec0dce689acc92e32d
Mag1sstr <danilkarachev234@mail.ru>
2025-01-02 20:36:07 +0500

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,45 +1,76 @@
-import { useState } from "react";
-import "./Modal.css";
-import { IDaysArr } from "../interfaces/interfaces";
+import { useState } from "react"
+import "./Modal.css"
+import { IDaysArr } from "../interfaces/interfaces"
+import { useAppDispatch, useAppSelector } from "../../store/store"
+import { setEvent } from "../../store/slices/calendarSlice"
 
 interface IProps {
-  style: string | null;
-  setClickModal: (bool: boolean) => void;
-  clickIndex: number;
-  setArray: (obj: IDaysArr[]) => void;
-  daysArr: IDaysArr[];
+   style: string | null
+   setClickModal: (bool: boolean) => void
+   clickIndex: number
+   setArray: (obj: IDaysArr[]) => void
+   daysArr: IDaysArr[]
+   currentYear: number
+   currentMonth: string
 }
 export default function Modal({
-  style,
-  setClickModal,
-  daysArr,
-  clickIndex,
-  setArray,
+   style,
+   setClickModal,
+   daysArr,
+   clickIndex,
+   setArray,
+   currentMonth,
+   currentYear,
 }: IProps) {
-  const [value, setValue] = useState("");
-  return (
-    <div onClick={() => setClickModal(false)} className={`modal ${style}`}>
-      <div onClick={(e) => e.stopPropagation()} className="modal__inner">
-        <h1 className="modal__title">
-          Создать событие на {clickIndex + 1} число
-        </h1>
-        <input
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              let copy: IDaysArr[] = [...daysArr];
-              copy[clickIndex].title = value;
-              setArray(copy);
-              setClickModal(false);
-              setValue("");
-            }
-          }}
-          className="modal__input"
-          type="text"
-          placeholder="Название события"
-        />
+   const [value, setValue] = useState("")
+   const dispatch = useAppDispatch()
+   const events = useAppSelector((state) => state.calendar.events)
+
+   //  function findDublicateMonth(copy) {
+   //     events.forEach((item) => {
+   //        if (item.month === currentMonth && item.year === currentYear) {
+   //           item.eventsArray = copy
+   //           return setArray(item.eventsArray)
+   //        }
+   //     })
+   //  }
+   //  console.log(events)
+
+   return (
+      <div onClick={() => setClickModal(false)} className={`modal ${style}`}>
+         <div onClick={(e) => e.stopPropagation()} className="modal__inner">
+            <h1 className="modal__title">
+               Создать событие на {clickIndex + 1} число
+            </h1>
+            <input
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+               onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                     let copy: IDaysArr[] = [...daysArr]
+                     copy[clickIndex] = {
+                        title: value,
+                        day: clickIndex + 1,
+                     }
+
+                     dispatch(
+                        setEvent({
+                           year: currentYear,
+                           month: currentMonth,
+                           eventsArray: copy,
+                        })
+                     )
+
+                     setArray(copy)
+                     setClickModal(false)
+                     setValue("")
+                  }
+               }}
+               className="modal__input"
+               type="text"
+               placeholder="Название события"
+            />
+         </div>
       </div>
-    </div>
-  );
+   )
 }
